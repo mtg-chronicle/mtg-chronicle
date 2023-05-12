6f9ad969cb613a25f226dab97758d5fab66f659a
mrbrique <locus.chan.dev@gmail.com>
2023-05-12 23:34:44 +0100

    implement footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 
 import { LandingComponent } from './components/landing/landing.component';
 import { UpcomingComponent } from './components/upcoming/upcoming.component';
@@ -28,6 +31,7 @@ import { SocialHubComponent } from './components/social-hub/social-hub.component
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     MatToolbarModule,
     MatButtonModule,
     MatInputModule,
