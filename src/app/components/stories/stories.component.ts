c58154904d4cf82e76c8cd9e17409ff3d3d39e09
mrbrique <locus.chan.dev@gmail.com>
2023-05-13 02:28:26 +0100

    fetch milestones from json

--- src/app/components/stories/stories.component.ts
+++ src/app/components/stories/stories.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Milestone } from 'src/app/shared/models/milestone.model';
 
 @Component({
   selector: 'mtg-stories',
@@ -7,7 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StoriesComponent implements OnInit {
 
-  constructor() { }
+  milestones: Milestone[] = [];
+
+  constructor(private http: HttpClient) {
+    this.http.get<Milestone[]>('/assets/json/milestones.json')
+      .subscribe(milestones => this.milestones = milestones);
+  }
 
   ngOnInit(): void {
   }
