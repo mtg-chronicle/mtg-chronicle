557c503dc838f1329141fe60990996e3b6245288
mrbrique <locus.chan.dev@gmail.com>
2023-05-16 13:06:36 +0100

    utilize ResponsiveService in app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,39 @@
-import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
+import { Component, HostBinding, OnInit } from '@angular/core';
 import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
+import { ResponsiveService } from './services/responsive.service';
+import { Viewsize } from './shared/models/viewsize.model';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  title = 'mtg-chronicle';
+
+  viewsize!: Viewsize;
 
   constructor(
     private matIconRegistry: MatIconRegistry,
-    private domSanitizer: DomSanitizer
+    private domSanitizer: DomSanitizer,
+    private responsiveService: ResponsiveService
   ) {
     this.matIconRegistry.addSvgIcon('angular-solid',
       this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/images/angular_solidBlack.svg")
     );
+  }
+
+  ngOnInit(): void {
+    this.responsiveService.getViewsize().subscribe(viewsize => this.viewsize = viewsize);
+  }
+
+
+  @HostBinding('class')
+  get viewsizeCategory(): string {
+    return this.viewsize.category;
   }
 
 
