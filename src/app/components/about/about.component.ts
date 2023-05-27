f4277d24cad8f526dba427c2bf8286d537e7bd43
mrbrique <locus.chan.dev@gmail.com>
2023-05-27 01:06:30 +0100

    implement about.component

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { ApplicationService } from 'src/app/services/application.service';
 
+import { Component, OnInit } from '@angular/core';
+import { ResponsiveComponent } from '../responsive/responsive.component';
+import { ResponsiveService } from 'src/app/services/responsive.service';
 @Component({
   selector: 'mtg-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
-export class AboutComponent implements OnInit {
+export class AboutComponent extends ResponsiveComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    responsiveService: ResponsiveService,
+    public appService: ApplicationService
+  ) {
+    super(responsiveService);
   }
+
 
 }
